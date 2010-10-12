ce1a2248947e364afb47900b9d321a376142d4a6
s.mardine <s.mardine@651ab0fa-97ec-0544-931e-a578820599c3>
2010-10-12 08:40:13 +0000

    premier post

---
+++ trunk/ layoutavecscrollviewvertical --username s.mardine/TestLayoutAvecScrollView/src/fr/smardine/layoutscrollview/produit.java
@@ -0,0 +1,11 @@
+package fr.smardine.layoutscrollview;
+
+public class produit {
+	String nom;
+	String detail;
+	
+	public produit(String _nom, String _detail) {
+		nom = _nom;
+		detail = _detail;
+	}
+}
